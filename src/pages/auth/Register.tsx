8abb7f53f1158688710e457e8a686b051d52005d
TrieuVanBan <79831622+TrieuVanBan@users.noreply.github.com>
2023-03-16 22:42:24 +0700

    up

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React from "react";
+import { Button, Checkbox, Form, Input } from "antd";
 import { Link, useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
-import { userType } from '../../type/user';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { userType } from "../../type/user";
 
 const Register = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const onFinish = (values: userType) => {
-    // console.log('Success:', values);
-    if(values.password === values.repass){
-      axios.post('http://localhost:3000/users', values)
-      .then(res => {
-        alert("Bạn đã đăng ký thành công!")
-        navigate('/signin')
-      }).catch(err => console.log(err))
-    }else{
-      console.log("Mật khẩu không trùng khớp !");
-    }
+    const { name, password, phone } = values;
+    const data = {
+      name,
+      password,
+      phone,
+    };
+    axios
+      .post("http://localhost:3000/users", data)
+      .then((res) => {
+        alert("Bạn đã đăng ký thành công!");
+        navigate("/signin");
+      })
+      .catch((err) => console.log(err));
   };
-  
+
   const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
 
   const validateMessages = {
@@ -35,63 +37,80 @@ const Register = () => {
 
   return (
     <>
-    <h2 style={{textAlign:'center'}}>Đăng ký</h2>
-    <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600, margin: '50px auto' }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-    validateMessages={validateMessages}
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[{ required: true}]}
-    >
-      <Input />
-    </Form.Item>
+      <h2 style={{ textAlign: "center" }}>Đăng ký</h2>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600, margin: "50px auto" }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        validateMessages={validateMessages}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
 
-    <Form.Item
-      label="PhoneNumber"
-      name="phone"
-      rules={[{ required: true, type: "number"}]}
-    >
-      <Input />
-    </Form.Item>
+        <Form.Item
+          label="PhoneNumber"
+          name="phone"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
 
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[{ required: true}]}
-    >
-      <Input.Password />
-    </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true }]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
 
-    <Form.Item
-      label="Repass"
-      name="repass"
-      rules={[{ required: true}]}
-    >
-      <Input.Password />
-    </Form.Item>
+        <Form.Item
+          label="Repass"
+          name="repass"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            { required: true },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error("Mật khẩu không trùng khớp!"));
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-    <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-      <Checkbox>Remember me</Checkbox>
-      <Link to={"/signin"}>Đăng nhập</Link>
-    </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+          <Link to={"/signin"}>Đăng nhập</Link>
+        </Form.Item>
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Đăng ký
-      </Button>
-    </Form.Item>
-  </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Đăng ký
+          </Button>
+        </Form.Item>
+      </Form>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -1,58 +1,76 @@
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const onFinish = (values: any) => {
-  console.log('Success:', values);
+  //localhost:3000/users
+  axios
+    .get(`localhost:3000/users/phone=${values.phone}`)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
 };
 
 const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
+  console.log("Failed:", errorInfo);
 };
 
 const Signin = () => {
   return (
     <>
-      <h2 style={{textAlign:'center'}}>Đăng nhập</h2>
+      <h2 style={{ textAlign: "center" }}>Đăng nhập</h2>
       <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600, margin: '50px auto' }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="PhoneNumber"
-        name="phone"
-        rules={[{ required: true, message: 'Không để trống số điện thoại!' }]}
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600, margin: "50px auto" }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="PhoneNumber"
+          name="phone"
+          rules={[{ required: true, message: "Không để trống số điện thoại!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Không để trống mật khẩu!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Không để trống mật khẩu!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
-        <Link to={"/register"}>Đăng ký</Link>
-      </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+          <Link to={"/register"}>Đăng ký</Link>
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Đăng nhập
-        </Button>
-      </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Đăng nhập
+          </Button>
+        </Form.Item>
       </Form>
     </>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
